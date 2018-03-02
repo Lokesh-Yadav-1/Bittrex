01ad89834be23303ffcd661930a31f7a52a209b8
lokesh <lokeshmailbox02@gmail.com>
2018-03-02 19:49:10 +0530

    tabs

--- app/src/main/java/com/example/lokesh/bittrexcryptoexchange/MainActivity.java
+++ app/src/main/java/com/example/lokesh/bittrexcryptoexchange/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.lokesh.bittrexcryptoexchange;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SectionsPageAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSectionsPagerAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        mViewPager = findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+
+    }
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter((getSupportFragmentManager()));
+        adapter.addFragment(new MarketFragment(),"Market");
+        viewPager.setAdapter(adapter);
     }
 }
