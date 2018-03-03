42b00db308b9ca8020566bbcb387c6cb0e3095ce
lokesh <lokeshmailbox02@gmail.com>
2018-03-03 21:51:23 +0530

    cardview tabs2

--- app/src/main/java/com/example/lokesh/bittrexcryptoexchange/MainActivity.java
+++ app/src/main/java/com/example/lokesh/bittrexcryptoexchange/MainActivity.java
@@ -1,26 +1,35 @@
 package com.example.lokesh.bittrexcryptoexchange;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TableLayout;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private SectionsPageAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mSectionsPagerAdapter = new SectionsPageAdapter(getSupportFragmentManager());
         mViewPager = findViewById(R.id.container);
         setupViewPager(mViewPager);
-
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
     }
     private void setupViewPager(ViewPager viewPager){
         SectionsPageAdapter adapter = new SectionsPageAdapter((getSupportFragmentManager()));
         adapter.addFragment(new MarketFragment(),"Market");
+        adapter.addFragment(new AccountFragment(),"Account" );
         viewPager.setAdapter(adapter);
     }
 }
