234bc56514e81748b35d255dedbf22932454741c
lokesh <lokeshmailbox02@gmail.com>
2018-03-05 01:12:29 +0530

    one activity two fragments with buttn click

--- app/src/main/java/com/example/lokesh/bittrexcryptoexchange/MainActivity.java
+++ app/src/main/java/com/example/lokesh/bittrexcryptoexchange/MainActivity.java
@@ -1,25 +1,63 @@
 package com.example.lokesh.bittrexcryptoexchange;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private SectionsPageAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
     private Toolbar toolbar;
     private TabLayout tabLayout;
+    private Button marketButtom,accountButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        marketButtom = findViewById(R.id.marketButton);
+        accountButton = findViewById(R.id.accountButton);
+
+
+        MarketFragment marketFragment = new MarketFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, marketFragment).commit();
+
+        marketButtom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MarketFragment marketFragment = new MarketFragment();
+                FragmentTransaction tranx = getSupportFragmentManager().beginTransaction();
+                tranx.replace(R.id.fragment_container, marketFragment);
+                tranx.addToBackStack(null);
+                tranx.commit();
+            }
+        });
+
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AccountFragment accountFragment = new AccountFragment();
+                FragmentTransaction tranx = getSupportFragmentManager().beginTransaction();
+                tranx.replace(R.id.fragment_container, accountFragment);
+                tranx.addToBackStack(null);
+                tranx.commit();
+            }
+        });
+
+ /*       toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mSectionsPagerAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+       mSectionsPagerAdapter = new SectionsPageAdapter(getSupportFragmentManager());
         mViewPager = findViewById(R.id.container);
         setupViewPager(mViewPager);
         tabLayout = findViewById(R.id.tabs);
@@ -28,9 +66,11 @@ public class MainActivity extends AppCompatActivity {
     }
     private void setupViewPager(ViewPager viewPager){
         SectionsPageAdapter adapter = new SectionsPageAdapter((getSupportFragmentManager()));
-        adapter.addFragment(new BitcoinFragment(),"Bitcoin Market");
+        adapter.addFragment(new MarketFragment(),"Bitcoin Market");
         adapter.addFragment(new ETHFragment(),"Ether Market");
         adapter.addFragment(new USDFragment(),"USD Market");
         viewPager.setAdapter(adapter);
+    }  */
     }
+
 }
--- app/src/main/java/com/example/lokesh/bittrexcryptoexchange/MarketFragment.java
+++ app/src/main/java/com/example/lokesh/bittrexcryptoexchange/MarketFragment.java
@@ -1,7 +1,10 @@
 package com.example.lokesh.bittrexcryptoexchange;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +12,33 @@ import android.view.ViewGroup;
 
 
 public class MarketFragment extends Fragment {
-
+    private SectionsPageAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+    private TabLayout tabLayout;
+    private Toolbar toolbar;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_market, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_market, container, false);
+        toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mSectionsPagerAdapter = new SectionsPageAdapter(getChildFragmentManager());
+        mViewPager = rootView.findViewById(R.id.containerMarket);
+        setupViewPager(mViewPager);
+        tabLayout = rootView.findViewById(R.id.tabsMarket);
+        tabLayout.setupWithViewPager(mViewPager);
+        return rootView;
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter((getChildFragmentManager()));
+        adapter.addFragment(new BitcoinFragment(),"Bitcoin Market");
+        adapter.addFragment(new ETHFragment(),"Ether Market");
+        adapter.addFragment(new USDFragment(),"USD Market");
+        viewPager.setAdapter(adapter);
     }
 
 }
